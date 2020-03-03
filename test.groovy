078ef99fe1fcc4855917f34a56dabd82933738c7
Taranjit K <Taranjitk18@gmail.com>
2020-03-02 20:55:50 -0500

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,19 +1,8 @@
-@Grapes([
-    @Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7'),
-    @Grab('org.apache.httpcomponents:httpmime:4.5.1')
-])
-import groovyx.net.http.HTTPBuilder;
 
 def firstTest() {
-
-         def baseUrl = new URL('https://jsonplaceholder.typicode.com/posts')
-         HttpURLConnection connection = (HttpURLConnection) baseUrl.openConnection();
-         connection.addRequestProperty("Accept", "application/json")
-         connection.with {
-           doOutput = true
-           requestMethod = 'GET'
-           println content.text
-         }
+    def response = httpRequest "https://jsonplaceholder.typicode.com/posts"
+    println('Status: '+response.status)
+    println('Response: '+response.content)
 }
 
 return this
