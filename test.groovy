de703ab54d84d64c5edee9eb17fa8adcf62002dc
Taranjit K <Taranjitk18@gmail.com>
2020-03-02 19:49:43 -0500

    First commit

---
+++ test.groovy
@@ -0,0 +1,15 @@
+import groovyx.net.http.HTTPBuilder;
+
+def firstTest() {
+
+         def baseUrl = new URL('https://jsonplaceholder.typicode.com/posts')
+         HttpURLConnection connection = (HttpURLConnection) baseUrl.openConnection();
+         connection.addRequestProperty("Accept", "application/json")
+         connection.with {
+           doOutput = true
+           requestMethod = 'GET'
+           println content.text
+         }
+}
+
+return this
